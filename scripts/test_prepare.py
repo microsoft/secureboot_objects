f3021be35f3459092dfaa2bb8be7c1f9ff7ddc4a
Joey Vagedes <joeyvagedes@microsoft.com>
2023-10-23 08:54:14 -0700

    Add testing infrastructure

---
+++ scripts/test_prepare.py
@@ -0,0 +1,8 @@
+# @file test_prepare.py
+# This file contains unit tests for prepare.py
+##
+# Copyright (c) Microsoft Corporation.
+#
+# SPDX-Licese-Identifier: BSD-2-Clause-Patent
+##
+"""Unit tests for prepare.py."""
---
+++ scripts/test_secure_boot_default_keys.py
@@ -0,0 +1,8 @@
+# @file test_secure_boot_default_keys.py
+# This file contains unit tests for secure_boot_default_keys.py
+##
+# Copyright (c) Microsoft Corporation.
+#
+# SPDX-Licese-Identifier: BSD-2-Clause-Patent
+##
+"""Unit tests for secure_boot_default_keys.py."""
