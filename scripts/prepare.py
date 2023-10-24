91585b3d3dea5843a97985b4b19f5b80dfa2e1ff
Doug Flick <dougflick@microsoft.com>
2023-10-24 01:17:59 -0400

    Initial contents (#6)
    This builds on Javagedes
    initial port and adds all the secure boot certificates provided by
    Microsoft.
    
    Performs an initial port of the project into the new repository. This
    port includes two scripts used to (1) generate the secure boot binaries
    for Intel and ARM architectures and (2) archive the binaries as .zip and
    .tar.gz files for each of the architectures.
    
    Adds auxillary files for performing PR checks and automated attachment
    of the archive files to the release, if the workflow is being run due to
    the creation of a tag.
    
    
    @[Javagedes](https://github.com/microsoft/secureboot_objects/commits?author=Javagedes)
    have done verification of PR Checks and Asset loading on release on my
    personal fork:
    https://github.com/Javagedes/secureboot_objects/actions/runs/5893194292
    
    ---------
    
    Signed-off-by: Joey Vagedes <joeyvagedes@microsoft.com>
    Co-authored-by: Joey Vagedes <joey.vagedes@gmail.com>
    Co-authored-by: Joey Vagedes <joeyvagedes@microsoft.com>
    Co-authored-by: Michael Kubacki <michael.kubacki@microsoft.com>

---
+++ scripts/prepare.py
@@ -0,0 +1,56 @@
+# @file
+#
+# Copyright (c) Microsoft Corporation
+# SPDX-License-Identifier: BSD-2-Clause-Patent
+##
+"""A command line script to prepare the files generated from secure_boot_default_keys.py for a github release."""
+import argparse
+import logging
+import pathlib
+import shutil
+import sys
+import tempfile
+
+LAYOUT = {
+    "edk2-arm-secureboot-binaries": "Arm",
+    "edk2-aarch64-secureboot-binaries": "Aarch64",
+    "edk2-ia32-secureboot-binaries": "Ia32",
+    "edk2-x64-secureboot-binaries": "X64",
+}
+
+def main() -> int:
+    """Entry point for the script."""
+    parser = argparse.ArgumentParser(
+        description="Organizes and zips the files for a release.")
+    parser.add_argument("input", type=pathlib.Path,
+                        help="The directory containing the files to be Prepared.")
+    parser.add_argument("--version", required=True,
+                        help="The version number of the release.")
+    parser.add_argument("-o","--output", default="Archives", type = pathlib.Path,
+                        help="The output directory for prepared files.")
+    args = parser.parse_args()
+
+    out_path = args.output
+    in_path = args.input
+    # Make directory if it doesn't exist. Delete any files in it if it does.
+    out_path.mkdir(parents=True, exist_ok=True)
+    for file_path in out_path.rglob("*"):
+        if file_path.is_file():
+            file_path.unlink()
+
+    for name, arch in LAYOUT.items():
+        tmp_dir = tempfile.TemporaryDirectory()
+        pathlib.Path(tmp_dir.name, "version").write_text(args.version)
+        if not (in_path / arch).exists():
+            raise RuntimeError(f"Missing {arch} directory in {in_path}")
+        shutil.copytree(in_path / arch, pathlib.Path(tmp_dir.name), dirs_exist_ok=True)
+
+        shutil.make_archive(out_path / name, "zip", tmp_dir.name)
+        shutil.make_archive(out_path / name, "gztar", tmp_dir.name)
+
+
+if __name__ == "__main__":
+
+    logging.basicConfig(level=logging.INFO,
+                        format="%(levelname)s: %(message)s")
+    sys.exit(main())
