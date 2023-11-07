58362cb02c41c4461f083f8267def934018cac40
Michael Kubacki <michael.kubacki@microsoft.com>
2023-11-06 21:43:36 -0500

    Add Secure Boot Objects license to generated readme files
    Updates secure_boot_default_keys.py to include the data license in
    the release archives so it is carried with distribution.
    
    Signed-off-by: Michael Kubacki <michael.kubacki@microsoft.com>

--- scripts/secure_boot_default_keys.py
+++ scripts/secure_boot_default_keys.py
@@ -281,6 +281,63 @@ for more information on key requirements if appending to the defaults provided i
                 continue
             if file_dict.get("url", None) is not None:
                 readme += f"* <{file_dict['url']}>\n"
+
+    readme += "\n---\n\n"
+
+    readme += """## License
+
+Terms of Use for Microsoft Secure Boot Objects ("Secure Boot Objects")
+
+By downloading the Secure Boot Objects, you agree to the following terms.
+If you do not accept them, do not download or use the Secure Boot Objects.
+
+These terms do not provide you with any legal rights to any intellectual
+property in any Microsoft product.
+
+You may copy and use the Secure Boot Objects for your internal, reference
+purposes and to design, develop, and test your software, firmware or hardware,
+as applicable; and you may distribute the Secure Boot Objects to end users
+solely as part of the distribution of an operating system software product, or
+as part of the distribution of updates to an operating system software product;
+and you may distribute the Secure Boot Objects to end users or through your
+distribution channels solely as embodied in a firmware product or hardware
+product that embodies nontrivial additional functionality. Without limiting the
+foregoing, copying or reproduction of the Secure Boot Objects to any other
+server or location for further reproduction or redistribution on a standalone
+basis is expressly prohibited.
+
+If you are engaged in the business of developing and commercializing hardware
+products that include the UEFI standard
+(available at https://uefi.org/specifications), you may copy and use the Secure
+Boot Objects for your internal, reference purposes and to design, develop, and
+test your software; and you may distribute the Secure Boot Objects end users
+solely as part of the distribution of an operating system software product, or
+as part of the distribution of updates to an operating system software product.
+Without limiting the foregoing, copying or reproduction of the Secure Boot
+Objects to any other server or location for further reproduction or
+redistribution on a standalone basis is expressly prohibited.
+The Secure Boot Objects are provided “as-is.” The information contained in the
+Secure Boot Objects may change without notice.  Microsoft does not represent
+that the Secure Boot Objects is error free and you bear the entire risk of
+using it.  NEITHER MICROSOFT NOR UEFI MAKES ANY WARRANTIES, EXPRESS OR IMPLIED,
+WITH RESPECT TO THE SECURE BOOT OBJECTS, AND MICROSOFT AND UEFI EACH EXPRESSLY
+DISCLAIMS ALL OTHER EXPRESS, IMPLIED, OR STATUTORY WARRANTIES.  THIS INCLUDES
+THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, TITLE AND
+NON-INFRINGEMENT.
+TO THE MAXIMUM EXTENT PERMITTED BY APPLICABLE LAW, IN NO EVENT SHALL MICROSOFT
+OR UEFI BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY
+DAMAGES WHATSOEVER ARISING OUT OF OR IN CONNECTION WITH THE USE OR DISTRIBUTION
+OF THE SECURE BOOT OBJECTS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+OTHER TORTIOUS ACTION.
+
+YOU AGREE TO RELEASE MICROSOFT (INCLUDING ITS AFFLIATES, CONTRACTORS, AGENTS,
+EMPLOYEES, LICENSEES AND ASSIGNEES) AND UEFI (INCLUDING ITS AFFILIATES,
+CONTRACTORS, AGENTS, EMPLOYEES, LICENSEES AND SUCCESSORS) FROM ANY AND ALL
+CLAIMS OR LIABILITY ARISING OUT OF YOUR USE OR DISTRIBUTION OF THE SECURE
+BOOT OBJECTS AND ANY RELATED INFORMATION.""" # noqa: E501
+
+    readme += "\n"
+
     return bytes(readme, "utf-8")
 
 def main() -> int:
